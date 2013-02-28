fb625ebb8b417b8726ed0fcd2e4ea291fb0e1ae9
bigboss-ps3dev <ajramosmarquez@gmail.com>
2013-02-28 17:46:57 +0100

    updated sce profile

--- sce_profile.sh
+++ sce_profile.sh
@@ -1,5 +1,5 @@
 CELL_SDK=/usr/local/cell;export CELL_SDK
 CELL_PSGL_VERSION=debug;export CELL_PSGL_VERSION
 SCE_PS3_ROOT=/usr/local/cell;export SCE_PS3_ROOT 
-PATH=$CELL_SDK/host-osx/bin:/usr/local/cell/host-osx/ppu/bin:/usr/local/cell/host-osx/spu/bin:$PATH;export PATH
+PATH=$CELL_SDK/host-osx/bin:$CELL_SDK/host-osx/ppu/bin:$CELL_SDK/host-osx/spu/bin:$PATH;export PATH
 CELL_BUILD_TOOLS=GCC;export CELL_BUILD_TOOLS
